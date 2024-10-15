45068c10e564e50238514d348eb58660f7f41b35
logn145236 <logn145236@gmail.com>
2024-10-15 22:06:36 +0700

    bổ sung xử ly đăng xuất thì không cho đặt phòng

--- src/components/templates/DetailRoomTemplate.tsx
+++ src/components/templates/DetailRoomTemplate.tsx
@@ -27,49 +27,52 @@ export const DetailRoomTemplate = () => {
   const [userId, setUserId] = useState<string>("0");
 
   const { id } = useParams();
-
-  useEffect(() => {
-    const userData = localStorage.getItem("USER");
-    // Lấy ID từ localStorage khi component mount
-    const storedUser = userData ? JSON.parse(userData) : null;
-    setUserId(storedUser.user.id); // Lưu ID vào state
-  }, []); // Dependency array trống để chỉ chạy một lần khi component được mount
-
-  let { data: reserListData }: { data?: ReservationResponse } = useQuery({
-    queryKey: ["GetDatPhongByUser", userId],
-    queryFn: () => datphongServices.getDetailReservationByUser(userId),
-  });
-
-  // Kiểm tra và chuyển đổi kiểu cho reserListData2
-  const reserListData2 = reserListData?.data?.content as
-    | Record<string, any>
-    | undefined;
-
+  let userData: any = null;
   let itemFound = false; // Tạo một flag để theo dõi xem đã tìm thấy item hay chưa
   let myObject = []; // Định nghĩa myObject là một mảng các đối tượng Item
-  if (reserListData2) {
-    console.log("reserListData2: ", reserListData2);
-    // Kiểm tra xem reserListData2 có tồn tại hay không
-    for (const key in reserListData2) {
-      for (const innerKey in reserListData2[key]) {
-        if (innerKey === "maPhong") {
-          let temp = reserListData2[key][innerKey];
-          if (Number(temp) === Number(id)) {
-            console.log("Found item with id: ", temp);
-            itemFound = true;
-            break; // Nếu tìm thấy item, dừng vòng lặp
+  useEffect(() => {
+    userData = localStorage.getItem("USER");
+    // Lấy ID từ localStorage khi component mount
+    if (userData) {
+      const storedUser = userData ? JSON.parse(userData) : null;
+      setUserId(storedUser.user.id); // Lưu ID vào state
+    }
+  }, []); // Dependency array trống để chỉ chạy một lần khi component được mount
+
+  if (!userData) {
+    let { data: reserListData }: { data?: ReservationResponse } = useQuery({
+      queryKey: ["GetDatPhongByUser", userId],
+      queryFn: () => datphongServices.getDetailReservationByUser(userId),
+    });
+    // Kiểm tra và chuyển đổi kiểu cho reserListData2
+    const reserListData2 = reserListData?.data?.content as
+      | Record<string, any>
+      | undefined;
+
+    if (reserListData2) {
+      console.log("reserListData2: ", reserListData2);
+      // Kiểm tra xem reserListData2 có tồn tại hay không
+      for (const key in reserListData2) {
+        for (const innerKey in reserListData2[key]) {
+          if (innerKey === "maPhong") {
+            let temp = reserListData2[key][innerKey];
+            if (Number(temp) === Number(id)) {
+              console.log("Found item with id: ", temp);
+              itemFound = true;
+              break; // Nếu tìm thấy item, dừng vòng lặp
+            }
           }
         }
-      }
-      if (itemFound) {
-        myObject.push(reserListData2[key]); // Thêm đối tượng vào mảng
-        itemFound = false;
+        if (itemFound) {
+          myObject.push(reserListData2[key]); // Thêm đối tượng vào mảng
+          itemFound = false;
+        }
       }
     }
-  }
-  console.log("myObject", myObject);
+    console.log("myObject", myObject);
 
-  console.log("userId", userId);
+    console.log("userId", userId);
+  }
 
   const { data: roomListData } = useQuery({
     queryKey: ["DetailRoom", id],
@@ -152,107 +155,124 @@ export const DetailRoomTemplate = () => {
                 Giá: {room.giaTien.toLocaleString("vi-VN")} VND / đêm
               </p>
             </div>
+            {userData && (
+              <>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <Controller
+                    name="id"
+                    control={control}
+                    render={({ field }) => (
+                      <Input type="hidden" {...field} value={0} /> // Đảm bảo newId luôn có giá trị
+                    )}
+                  />
+                  <Controller
+                    name="maPhong"
+                    control={control}
+                    render={({ field }) => (
+                      <input type="hidden" {...field} value={0} /> // Đảm bảo room.id luôn có giá trị
+                    )}
+                  />
+                  <div className="mb-4">
+                    <label className="block text-gray-700 mb-1">
+                      Nhận phòng
+                    </label>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Controller
-                name="id"
-                control={control}
-                render={({ field }) => (
-                  <Input type="hidden" {...field} value={0} /> // Đảm bảo newId luôn có giá trị
-                )}
-              />
-              <Controller
-                name="maPhong"
-                control={control}
-                render={({ field }) => (
-                  <input type="hidden" {...field} value={0} /> // Đảm bảo room.id luôn có giá trị
-                )}
-              />
-              <div className="mb-4">
-                <label className="block text-gray-700 mb-1">Nhận phòng</label>
-
-                <Controller
-                  name="ngayDen"
-                  control={control}
-                  render={({ field }) => (
-                    <DatePicker
-                      {...field}
-                      format="DD/MM/YYYY"
-                      value={
-                        field.value ? moment(field.value, "DD/MM/YYYY") : null
-                      }
-                      onChange={(date) =>
-                        field.onChange(date ? date.format("DD/MM/YYYY") : null)
-                      }
+                    <Controller
+                      name="ngayDen"
+                      control={control}
+                      render={({ field }) => (
+                        <DatePicker
+                          {...field}
+                          format="DD/MM/YYYY"
+                          value={
+                            field.value
+                              ? moment(field.value, "DD/MM/YYYY")
+                              : null
+                          }
+                          onChange={(date) =>
+                            field.onChange(
+                              date ? date.format("DD/MM/YYYY") : null
+                            )
+                          }
+                        />
+                      )}
                     />
-                  )}
-                />
-                {errors.ngayDen && (
-                  <p className="text-red-500">{errors.ngayDen.message}</p>
-                )}
-              </div>
+                    {errors.ngayDen && (
+                      <p className="text-red-500">{errors.ngayDen.message}</p>
+                    )}
+                  </div>
 
-              <div className="mb-4">
-                <label className="block text-gray-700 mb-1">Trả phòng</label>
+                  <div className="mb-4">
+                    <label className="block text-gray-700 mb-1">
+                      Trả phòng
+                    </label>
 
-                <Controller
-                  name="ngayDi"
-                  control={control}
-                  render={({ field }) => (
-                    <DatePicker
-                      {...field}
-                      format="DD/MM/YYYY"
-                      value={
-                        field.value ? moment(field.value, "DD/MM/YYYY") : null
-                      }
-                      onChange={(date) =>
-                        field.onChange(date ? date.format("DD/MM/YYYY") : null)
-                      }
+                    <Controller
+                      name="ngayDi"
+                      control={control}
+                      render={({ field }) => (
+                        <DatePicker
+                          {...field}
+                          format="DD/MM/YYYY"
+                          value={
+                            field.value
+                              ? moment(field.value, "DD/MM/YYYY")
+                              : null
+                          }
+                          onChange={(date) =>
+                            field.onChange(
+                              date ? date.format("DD/MM/YYYY") : null
+                            )
+                          }
+                        />
+                      )}
                     />
-                  )}
-                />
 
-                {errors.ngayDi && (
-                  <p className="text-red-500">{errors.ngayDi.message}</p>
-                )}
-              </div>
+                    {errors.ngayDi && (
+                      <p className="text-red-500">{errors.ngayDi.message}</p>
+                    )}
+                  </div>
 
-              <div className="mb-4">
-                <label className="block text-gray-700 mb-1">
-                  số lượng Khách
-                </label>
-                <Controller
-                  name="soLuongKhach"
-                  control={control}
-                  render={({ field }) => (
-                    <Input
-                      type="number"
-                      status={errors.soLuongKhach && "error"}
-                      {...field}
-                      value={field.value || 0} // Đảm bảo field.value luôn có giá trị
+                  <div className="mb-4">
+                    <label className="block text-gray-700 mb-1">
+                      số lượng Khách
+                    </label>
+                    <Controller
+                      name="soLuongKhach"
+                      control={control}
+                      render={({ field }) => (
+                        <Input
+                          type="number"
+                          status={errors.soLuongKhach && "error"}
+                          {...field}
+                          value={field.value || 0} // Đảm bảo field.value luôn có giá trị
+                        />
+                      )}
                     />
-                  )}
-                />
-                {errors.soLuongKhach && (
-                  <p className="text-red-500">{errors.soLuongKhach.message}</p>
-                )}
-              </div>
+                    {errors.soLuongKhach && (
+                      <p className="text-red-500">
+                        {errors.soLuongKhach.message}
+                      </p>
+                    )}
+                  </div>
 
-              <Controller
-                name="maNguoiDung"
-                control={control}
-                render={({ field }) => (
-                  <input type="hidden" {...field} value={0} /> // Đảm bảo userId luôn có giá trị
-                )}
-              />
-              <Button
-                htmlType="submit"
-                loading={reservationMutation.isPending}
-                className="bg-gradient-to-r from-red-500 to-pink-500 text-white font-bold py-2 px-4 rounded w-full"
-              >
-                Đặt phòng
-              </Button>
-            </form>
+                  <Controller
+                    name="maNguoiDung"
+                    control={control}
+                    render={({ field }) => (
+                      <input type="hidden" {...field} value={0} /> // Đảm bảo userId luôn có giá trị
+                    )}
+                  />
+                  <Button
+                    htmlType="submit"
+                    loading={reservationMutation.isPending}
+                    className="bg-gradient-to-r from-red-500 to-pink-500 text-white font-bold py-2 px-4 rounded w-full"
+                  >
+                    Đặt phòng
+                  </Button>
+                </form>
+              </>
+            )}
           </div>
         </Col>
       </Row>
@@ -260,25 +280,31 @@ export const DetailRoomTemplate = () => {
       <Row className="pt-4 cursor-default" gutter={[16, 16]}>
         <Col span={16}></Col>
         <Col span={8}>
-          {myObject && (
+          {userData && (
             <>
-              <h2>Danh sách đã đăng ký</h2>
-              <Row gutter={16}>
-                {myObject.map((room) => (
-                  <Row className="p-1">
-                    <Col span={8} key={room.id}>
-                      <Card hoverable style={{ width: 240 }}>
-                        <Meta
-                          title={`Phòng ${room.id}`}
-                          description={`Từ: ${moment(room.ngayDen).format(
-                            "DD/MM/YYYY"
-                          )} Đến: ${moment(room.ngayDi).format("DD/MM/YYYY")}`}
-                        />
-                      </Card>
-                    </Col>
+              {myObject && (
+                <>
+                  <h2>Danh sách đã đăng ký</h2>
+                  <Row gutter={16}>
+                    {myObject.map((room) => (
+                      <Row className="p-1">
+                        <Col span={8} key={room.id}>
+                          <Card hoverable style={{ width: 240 }}>
+                            <Meta
+                              title={`Phòng ${room.id}`}
+                              description={`Từ: ${moment(room.ngayDen).format(
+                                "DD/MM/YYYY"
+                              )} Đến: ${moment(room.ngayDi).format(
+                                "DD/MM/YYYY"
+                              )}`}
+                            />
+                          </Card>
+                        </Col>
+                      </Row>
+                    ))}
                   </Row>
-                ))}
-              </Row>
+                </>
+              )}
             </>
           )}
         </Col>
